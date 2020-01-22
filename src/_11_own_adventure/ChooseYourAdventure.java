038ae2c6ed30caf66db72725adae4aab1aee8efb
TechProgrammer123 <43911240+TechProgrammer123@users.noreply.github.com>
2020-01-22 14:55:33 -0800

    :smile:

--- src/_11_own_adventure/ChooseYourAdventure.java
+++ src/_11_own_adventure/ChooseYourAdventure.java
@@ -7,11 +7,11 @@ public static void main(String[] args) {
 	//Hide under a strong object = 0
 	//Use a fire extinguisher = 1
 	//Break and jump = 2
-	int option1 = JOptionPane.showOptionDialog(null, "You are in a earthquake in a building. You are on the 3th floor. You can't jump through the window because What will you do?", "Choose an option", 0, JOptionPane.INFORMATION_MESSAGE, null,
-			new String[] { "Hide under a object", "Use a fire extinguisher", "Break and jump through a window" }, null);
+	int option1 = JOptionPane.showOptionDialog(null, "You are on an unknown island with very little food and a water filter. What will you do?", "Choose an option", 0, JOptionPane.INFORMATION_MESSAGE, null,
+			new String[] { "Conquer a pirate ship", "Use the washed up boat", "Break and jump through a window" }, null);
 	System.out.println(option1);
 	if (option1 == 2) {
-		JOptionPane.showMessageDialog(null, "You have died because you had jumped into a river  of alligators surrounding the building exept the enterance ");
+		JOptionPane.showMessageDialog(null, "You have died because you had jumped into a ocean  of alligators surrounding the island ");
 				
 		
 		
